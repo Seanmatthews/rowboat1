7bd32ebe204cea8f00280eb21b919f6a944d453d
Sean D Matthews <sean@seans-MacBook-Pro.local>
2015-08-23 22:59:24 -0400

    base requirements after ubuntu install

---
+++ src/base-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt-get update
+
+sudo apt-get -y install git
+sudo apt-get -y install emacs
+sudo apt-get -y install build-essential
+sudo apt-get -y install python-dev
+
+#install pip
+sudo apt-get -y install python-pip
+
+exit
