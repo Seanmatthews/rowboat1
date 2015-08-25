d700cc59e336a0f6e55d350d5d985f0ebc99f23e
Sean D Matthews <sean@seans-MacBook-Pro.local>
2015-08-24 21:18:53 -0400

    added all basic ros install

--- src/base-install.sh
+++ src/base-install.sh
@@ -5,8 +5,17 @@ sudo apt-get -y install git
 sudo apt-get -y install emacs
 sudo apt-get -y install build-essential
 sudo apt-get -y install python-dev
+sudo apt-get -y install g++
 
-#install pip
-sudo apt-get -y install python-pip
+# ROS
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
+sudo apt-get update
+sudo apt-get install ros-jade-desktop-full
+sudo rosdep init
+rosdep update
+echo "source /opt/ros/jade/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+sudo apt-get -y install python-rosinstall
 
 exit
