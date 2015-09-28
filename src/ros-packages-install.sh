901917c9865e81a975f3125ab43366519af7af71
Sean D Matthews <sean@seans-MacBook-Pro.local>
2015-09-27 22:04:38 -0400

    added common workspace

--- src/ros-packages-install.sh
+++ src/ros-packages-install.sh
@@ -5,10 +5,13 @@ cd /vagrant/arbiter
 catkin_make
 cd /vagrant/navigator
 catkin_make
+cd /vagrant/common
+catkin_make
 
 # Add all workspaces to the path
 echo "/vagrant/safety/devel/setup.bash" >> ~/.bashrc
 echo "/vagrant/navigator/devel/setup.bash" >> ~/.bashrc
 echo "/vagrant/arbiter/devel/setup.bash" >> ~/.bashrc
+echo "/vagrant/common/devel/setup.bash" >> ~/.bashrc
 
 source ~/.bashrc
