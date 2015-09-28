61458cf35c682eddae99ae35c975c28ee560fa2f
Sean D Matthews <sean@seans-MacBook-Pro.local>
2015-09-27 22:42:37 -0400

    install i2c

--- src/install/base-install.sh
+++ src/install/base-install.sh
@@ -6,6 +6,8 @@ sudo apt-get -y install emacs
 sudo apt-get -y install build-essential
 sudo apt-get -y install python-dev
 sudo apt-get -y install g++
+sudo apt-get -y install python-smbus
+sudo apt-get -y install i2c-tools
 
 # ROS
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
--- src/install/navigator-install.sh
+++ src/install/navigator-install.sh
@@ -1,3 +1,9 @@
 # Install I2C driver 
 modprobe am1_i2c
+sudo echo "i2c-dev" >> /etc/modules
 sudo echo "am1_i2c" >> /etc/modules
+
+# Add odroid user to i2c group so we don't need route for i2c access
+sudo adduser odroid i2c
+
+sudo reboot
