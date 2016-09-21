b1e0d26c13c418731f5e2cea0bf3793881199374
Sean Matthews <sean.d.matthews@gmail.com>
2016-09-21 23:25:46 +0000

    Adds libcurlpp

--- src/install/base-install.sh
+++ src/install/base-install.sh
@@ -35,7 +35,7 @@ ${PKG} mono-devel
 ${PKG} libmono-winforms2.0-cil
 
 # FFMpeg -- ffmpeg not in Ubuntu 14.04. Change to apt-get if upgrading to 16.*
-$PKG} autoconf automake build-essential libass-dev libfreetype6-dev \
+${PKG} autoconf automake build-essential libass-dev libfreetype6-dev \
   libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev \
   libxcb-shm0-dev libxcb-xfixes0-dev pkg-config texinfo zlib1g-dev
 
@@ -47,5 +47,8 @@ make
 sudo make install
 cd ..
 rm -rf FFmpeg
+
+#
+${PKG} libcurlpp-dev libcurlpp0
 
 exit
