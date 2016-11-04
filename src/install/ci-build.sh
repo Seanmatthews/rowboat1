43ad97b561d0401796ce5469a3565634062b2a4a
Sean Matthews <sean.d.matthews@gmail.com>
2016-11-04 20:23:50 +0000

    Removes cruft

--- src/install/ci-build.sh
+++ src/install/ci-build.sh
@@ -3,10 +3,6 @@ set -ev
 source /opt/ros/indigo/setup.bash
 # extend this as needed for complete build coverage
 
-echo " ~~~~### INSTALLING DEPENDENCIES ###~~~~"
-# /root/rowboat1/src/install/base-install.sh
-# /root/rowboat1/src/rowboat/ros-packages-install.sh
-
 echo " ~~~~### BUILDING WORKSPACE ###~~~~"
 cd /root/rowboat1/src/rowboat
 git submodule update --init --recursive
