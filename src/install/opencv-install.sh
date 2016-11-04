5438d62c859db6c8b22ba8686982f300f03e3fb0
Sean Matthews <sean.d.matthews@gmail.com>
2016-11-04 17:33:19 +0000

    Reverting to travis build method

--- src/install/opencv-install.sh
+++ src/install/opencv-install.sh
@@ -1,12 +1,22 @@
 # Separated this install script out because it doesn't belong in the base-install script.
 # ALso, this takes a long time on the Odroid
 
+# OpenCV
+${PKG} libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+${PKG} python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+cd
 git clone https://github.com/Itseez/opencv.git
 git clone https://github.com/Itseez/opencv_contrib.git
-cd opencv
-git checkout tags/3.1.0
+cd opencv_contrib
+git checkout 3.1.0
+cd ../opencv
+git checkout 3.1.0
 mkdir build
 cd build
-cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local -D OPENCV_EXTRA_MODULES_PATH=/root/opencv_contrib/modules ..
+cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules ..
 make -j6
 make install
+cd
+rm -rf opencv
+rm -rf opencv_contrib
+
