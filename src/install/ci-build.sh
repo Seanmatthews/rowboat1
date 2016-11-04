6cf959dd79486a01142af7c48f81c01b1e36de66
Sean Matthews <sean.d.matthews@gmail.com>
2016-11-04 20:22:10 +0000

    Copy .bash_profile to homedir

--- src/install/ci-build.sh
+++ src/install/ci-build.sh
@@ -7,21 +7,6 @@ echo " ~~~~### INSTALLING DEPENDENCIES ###~~~~"
 # /root/rowboat1/src/install/base-install.sh
 # /root/rowboat1/src/rowboat/ros-packages-install.sh
 
-tty_fix = \
-"if [[ $- == *i* ]]
-then 
-    NRM=`tput sgr0` 
-    BLD=`tput bold` 
-    ITL=`tput sitm` 
-    UL=`tput smul` 
-    RED=`tput setaf 1` 
-    GRN=`tput setaf 2` 
-    BLU=`tput setaf 4`
-    PS1='\n\r${BLD}\u${NRM}|${UL}\h${NRM} [${BLD}${BLU}\W${NRM}] \w \n>> '
-fi"
-
-echo "$tty_fix" > /root/.bash_profile 
-
 echo " ~~~~### BUILDING WORKSPACE ###~~~~"
 cd /root/rowboat1/src/rowboat
 git submodule update --init --recursive
